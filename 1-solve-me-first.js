87d007cd266f59b76c4d7f884254a86b61768c45
Achintha Isuru <achinthaisuru444@gmail.com>
2024-10-16 19:08:46 -0400

    updated he readme file

---
+++ 1-solve-me-first.js
@@ -0,0 +1,8 @@
+function solveMeFirst(a, b) {
+    return a + b;
+}
+
+/**
+ * In here the question is asking for the sum of two numbers, and we can achieve by the above function.
+ * The function takes two parameters, a and b, and returns their sum.
+ */
